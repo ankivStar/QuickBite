00f36494d7d6552b3cd6a4f9c8d4fd5769e55b56
Ankit <av542939@gmail.com>
2023-12-06 19:16:55 +0530

    creating nested html element

--- App.js
+++ App.js
@@ -1,4 +1,26 @@
-const heading = React.createElement("h1", {id : "heading", xyz: "jai ho"}, "Hello world from react!")
-console.log(heading);
+/* <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+    </div>
+</div> */
+
+const parent = React.createElement(
+    "div", 
+    {id : 'parent'}, 
+    React.createElement(
+        'div', 
+        {id : 'child'}, 
+        React.createElement('h1', {}, "'I'm h1 tag")
+    )
+);
+
+const heading = React.createElement(
+"h1", 
+{id : "heading", xyz: "jai ho"}, 
+"Hello world from react!");
+
+console.log(parent); // object
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+
+root.render(parent)
