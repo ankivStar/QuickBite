3e8c86ba39753e20c24428596db1a3ab8050a84d
Ankit <av542939@gmail.com>
2023-12-07 16:14:06 +0530

    adding dynamic data

--- App.js
+++ App.js
@@ -39,7 +39,8 @@ const Header = () => {
     ) 
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    console.log(props);
     return (
         <div className="res-card" style={{backgroundColor : "#f0f0f0"}}>
             <img
@@ -47,8 +48,8 @@ const RestaurantCard = () => {
                 alt="res-logo"
                 src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xqwpuhgnsaf18te7zvtv" 
             />
-            <h3>Meghna Foods</h3>
-            <h4>Biryani, North Indian, Asian</h4>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine }</h4>
             <h4>4.4 stars</h4>
             <h4>38 minutes</h4>
 
@@ -61,12 +62,8 @@ const Body = () => {
         <div className="body">
             <div className="search">Serach</div>  
             <div className="res-container">
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                <RestaurantCard resName="Meghna Foods" cuisine="Biryani, North Indian, Asian"/>
+                <RestaurantCard resName="KFC" cuisine="Burger, Fast Food"/>
             </div>
         </div>
     )
