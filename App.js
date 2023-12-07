cf24490e96d2ee6e882fdbe2c9e4f4c82a36e4be
Ankit <av542939@gmail.com>
2023-12-07 11:49:52 +0530

    react component and functional component

--- App.js
+++ App.js
@@ -3,8 +3,21 @@ import ReactDOM from "react-dom/client"
 
 //JSX -HTML-like or xml - like syntax
 // JSX (tranpiled before it reaches the js) - PARCEL - Babel
-const jsxHeading = <h1 id="heading">Namaste React using jsx</h1>
+const Title = () =>(<h1 id="heading">Namaste React using jsx</h1>)
+
+//React component
+// class based component-old
+// functional component-new
+
+// react functional component
+const HeadingComponent = ()=>(
+    <div id="conatainer">
+        <Title/>
+        <h2>{100+300}</h2>
+        <h1> Namaste React Functional  Component</h1> 
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(jsxHeading)
+root.render(<HeadingComponent />)
