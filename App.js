5ad6238562e4337aeb947a9b2db793f2dda1e877
Ankit <av542939@gmail.com>
2023-12-07 16:03:59 +0530

    started making food app

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import ReactDOM from "react-dom/client"
  * Body
  *  - Serach
  *  - card conatiner
- *      restaurantCard
+ *     - restaurantCard
+ *        - img
+ *        - Name of res, star rating, cuisines, delivery time
  * 
  * Footer
  * - copyright
@@ -19,7 +21,64 @@ import ReactDOM from "react-dom/client"
  * 
  */
 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container"> 
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    ) 
+};
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor : "#f0f0f0"}}>
+            <img
+                className="res-logo"
+                alt="res-logo"
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xqwpuhgnsaf18te7zvtv" 
+            />
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Serach</div>  
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<HeadingComponent />)
+root.render(<AppLayout/>)
