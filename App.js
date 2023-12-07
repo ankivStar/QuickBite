8eb81976b87762daa0818457fb97f7e438efa191
Ankit <av542939@gmail.com>
2023-12-07 12:19:46 +0530

    some more on jsx

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const Title = (<h1 id="heading">Namaste React using jsx</h1>)
+const elem = <span> React Element</span>
+
+const Title = () => (<h1 id="heading">
+    {elem}
+    Namaste React using jsx
+    </h1>
+)
 
 // react functional component
 const HeadingComponent = ()=>(
     <div id="conatainer">
-        {Title}
+        {Title()}
+        <Title />
+        <Title></Title>
         <h2>{100+300}</h2>
         <h1> Namaste React Functional  Component</h1> 
     </div>
