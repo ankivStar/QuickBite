88b8b1fe3458ff46e9504f474ba474ea83857951
Ankit <av542939@gmail.com>
2023-12-07 12:00:35 +0530

    writing any js expression inside component using {}

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-//JSX -HTML-like or xml - like syntax
-// JSX (tranpiled before it reaches the js) - PARCEL - Babel
-const Title = () =>(<h1 id="heading">Namaste React using jsx</h1>)
-
-//React component
-// class based component-old
-// functional component-new
+const Title = (<h1 id="heading">Namaste React using jsx</h1>)
 
 // react functional component
 const HeadingComponent = ()=>(
     <div id="conatainer">
-        <Title/>
+        {Title}
         <h2>{100+300}</h2>
         <h1> Namaste React Functional  Component</h1> 
     </div>
