04c76bceb989862382ce3f49fb48bbe076579692
Ankit <av542939@gmail.com>
2023-12-22 09:44:03 +0530

    created class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () =>{
     return (
         <div>
             <h1>About</h1>
             <h2>This is namaste react webseries</h2>
+            {/* <User name={"Ankit Verma (function)"} country={"India"}/> */}
+            <UserClass name={"Ankit Verma (class)"}/>
         </div>
     );
 };
 
-export default About;
+export default About; 
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -13,6 +13,24 @@ const Shimmer = () =>{
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
         <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
     </div>)
 }
 export default Shimmer;
