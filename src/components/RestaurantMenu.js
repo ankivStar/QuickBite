87e3909a7e47e543d218f9172d25c216174f8c9a
Ankit <av542939@gmail.com>
2023-12-26 12:23:07 +0530

    created custom hook useRestaurantMenu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () =>{
 
-    const [resInfo, setResInfo ] = useState(null);
-
     const {resId} = useParams();
 
-    useEffect(() => {
-        fetchMenue();
-    },[]);
-
-    const fetchMenue = async () => {
-        try{
-            const data = await fetch(MENU_API+resId);
-            const json = await data.json();  
-            // console.log(json); 
-            setResInfo(json.data)     
-        } catch (error){
-            console.log(error);
-        }
-    };
+    const resInfo = useRestaurantMenu(resId);
 
     if(resInfo === null) return <Shimmer/>
     
@@ -41,7 +25,7 @@ const RestaurantMenu = () =>{
             totalItemArray.push(itemCards);
         }
     }
-    // console.log(totalItemArray);
+    console.log(totalItemArray);
 
     return (
         <div className="menu">
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,36 +1,45 @@
 const Shimmer = () =>{
     return(
-    <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+    <div className="body">
+        <div className="filter">
+            <div className="search">
+                <input className="search-box shimmer-input-style"/>
+                <input className="shimmer-input shimmer-input-style"/>
+            </div>
+            <input className="shimmer-rating shimmer-input-style"/>
+        </div> 
+        <div className="shimmer-container"> 
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
     </div>)
 }
 export default Shimmer;
