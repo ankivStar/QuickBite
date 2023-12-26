f273a2c023ada3869f409195e49c70252b4664be
Ankit <av542939@gmail.com>
2023-12-26 18:53:50 +0530

    corrected indentation

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,86 +6,92 @@ import useOnlineStatus from "../utils/useOnlineStatus.js";
  
 const Body = () => {
 
-    // local state variable - Super powerful variable
-    const [listOfReastaurant, setlistOfRestaurant] = useState([])
-    const [searchText, setSearchText] = useState("")
-    const [listOfFilteredRestaurant, setListOfFilteredRestaurant] = useState([]);
+  // local state variable - Super powerful variable
+  const [listOfReastaurant, setlistOfRestaurant] = useState([])
+  const [searchText, setSearchText] = useState("")
+  const [listOfFilteredRestaurant, setListOfFilteredRestaurant] = useState([]);
 
 
-    useEffect(()=>{
-      fetchData()
-    }, [])
+  useEffect(()=>{
+    fetchData()
+  }, [])
 
-    const fetchData = async  () =>{
-      try{
-        const data = await fetch(
-          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
+  const fetchData = async  () =>{
+    try{
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
 
-        const json = await data.json()
+      const json = await data.json()
 
-        async function checkJsonData(jsonData){
-          for(let i = 0; i < jsonData?.data?.cards?.length; i++){
-            let checkData = jsonData?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      async function checkJsonData(jsonData){
+        for(let i = 0; i < jsonData?.data?.cards?.length; i++){
+          let checkData = jsonData?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
-            if(checkData !== undefined){
-              return checkData;
-            }
+          if(checkData !== undefined){
+            return checkData;
           }
-        } 
+        }
+      } 
 
-        const allRestaurant = await checkJsonData(json);
-        setlistOfRestaurant(allRestaurant)
-        setListOfFilteredRestaurant(allRestaurant)
-      } catch(error) {
-        console.log(error);
-      }
-    };
+      const allRestaurant = await checkJsonData(json);
+      setlistOfRestaurant(allRestaurant)
+      setListOfFilteredRestaurant(allRestaurant)
+    } catch(error) {
+      console.log(error);
+    }
+  };
 
-    const onlineStatus = useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
-    if(onlineStatus === false) return (
-      <h1>
-        Looks like you are offline !! Please check your internet connection!
-      </h1>
-    )
+  if(onlineStatus === false) return (
+    <h1>
+      Looks like you are offline !! Please check your internet connection!
+    </h1>
+  )
 
-    return listOfReastaurant.length === 0?(
+  return listOfReastaurant.length === 0?
+  (
     <Shimmer/>
-    ):(
-        <div className="body">
-          <div className="filter">
-            <div className="search">
-                  <input type="text" className="search-box" value={searchText} onChange={(e)=>{
-                    setSearchText(e.target.value)
-                  }}/>
-                  <button onClick={()=>{
-                     // filter the restaurnt cards and update the UI
-                     const filteredRestaurant = listOfReastaurant.filter((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase()))  
+  ):
+  (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input type="text" className="border border-solid border-black" value={searchText} onChange={(e)=>{
+            setSearchText(e.target.value)
+          }}/>
+          <button className="px-4 bg-green-100 m-4 rounded-lg" onClick={()=>{
+            // filter the restaurnt cards and update the UI
+            const filteredRestaurant = listOfReastaurant.filter((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase()))  
 
-                     setListOfFilteredRestaurant(filteredRestaurant)
-                  }}>Search</button> 
-            </div>
-            <button className="filter-btn" onClick={()=>{
-            // filter logic here
-            const filteredRestaurant = listOfReastaurant.filter((res) => res.info.avgRating > 4);
-            setListOfFilteredRestaurant(filteredRestaurant);
+            setListOfFilteredRestaurant(filteredRestaurant)
+          }}>Search</button> 
+        </div>
+
+        <div className=" search m-4 p-4 flex items-center">
+          <button className="px-4 bg-gray-100 rounded-lg" onClick={()=>{
+          // filter logic here
+          const filteredRestaurant = listOfReastaurant.filter((res) => res.info.avgRating > 4);
+          setListOfFilteredRestaurant(filteredRestaurant);
           }}
           >
-            Top Rated Restaurant 
-            </button>
-          </div>  
-          <div className="res-container">
-              {
-                 listOfFilteredRestaurant.map((restaurant) => ( 
-                 <Link
-                 key={restaurant?.info?.id}
-                 to={"/restaurant/"+restaurant?.info?.id}>
-                  <RestaurantCard  resName = {restaurant}/>
-                 </Link>
-              ))} 
-          </div>
+          Top Rated Restaurant 
+          </button>
         </div>
-      )
+        
+      </div>  
+      <div className="flex">
+        {
+          listOfFilteredRestaurant.map((restaurant) => ( 
+          <Link
+          key={restaurant?.info?.id}
+          to={"/restaurant/"+restaurant?.info?.id}>
+          <RestaurantCard  resName = {restaurant}/>
+          </Link>
+        ))} 
+      </div>
+    </div>
+  )
 }
 export default Body;
