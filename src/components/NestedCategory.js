9c9aacf6a70311f56ef24a5701fb6328f6dc2d0e
Ankit <av542939@gmail.com>
2023-12-29 01:46:00 +0530

    removed unnecessary imports

--- src/components/NestedCategory.js
+++ src/components/NestedCategory.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { CDN_URL } from "../utils/constants";
-import ItemList from "./ItemList";
 import NestedRestaurantCategory from "./NestedRestaurantCategory";
 
 
